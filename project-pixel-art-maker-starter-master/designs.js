94a101135b808a15ba87fd1bb44304302c0e39ef
carys0331 <109781745+carys0331@users.noreply.github.com>
2022-09-29 11:43:27 +0900

    Update designs.js

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,36 +1,36 @@
 // Select color input
-const color = document.getElementById ('colorPicker');
-
+const color = document.getElementById("colorPicker");
 // Select size input
-const height = document.getElementById ('inputHeight');
-const width = document.getElementById ('inputWidth');
-const table = document.getElementById ('pixelCanvas');
-const size = document.getElementById ('sizePicker');
-
-
+const height = document.getElementById("inputHeight");
+const width = document.getElementById("inputWidth");
+const table = document.getElementById("pixelCanvas");
+const size = document.getElementById("sizePicker");
 // When size is submitted by the user, call makeGrid()
 size.addEventListener('submit', function(event) {
-    event.preventDefault();
-    makeGrid();
-  });
+	event.preventDefault();
+	makeGrid();
+})
 
 function makeGrid() {
-  table.innerHTML = '';
-  let tr,td;
-  
-  for (let row = 0; row < height; row++) {
-    tr = document.createElement('tr');
-    table.appendChild(tr);
-    for (let cell = 0; cell < width; cell++) {
-        td = document.createElement('td');
-        tr.appendChild(td);
-    }
-  }
-  
-    table.addEventListener('click', function(event) {
-    event.preventDefault();
-    if (event.target.tagName === 'TD') {
-        event.target.style.backgroundColor = color;
-    }
-  });
+	table.innerHTML = "";
+	let tr, td;
+
+	for (let row = 0; row < height.value; row++) {
+		let tableRow = document.createElement("tr");
+
+		for (let cell = 0; cell < width.value; cell++) {
+			let tableCell = document.createElement("td");
+			tableRow.appendChild(tableCell);
+		}
+		table.appendChild(tableRow);
+	}
+
+	table.addEventListener('click', function(event) {
+		event.preventDefault();
+		if (event.target.tagName === 'TD') {
+			event.target.style.backgroundColor = color.value;
+		}
+	});
 }
+
+makeGrid();
